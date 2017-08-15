ee976235a79c3fdd8df18be60a6ec79937da32be
Dan Falconer <github@crazedsanity.com>
2017-08-15 15:40:48 -0500

    Support PHP up to 7.2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-//echo "RUNNING (". __FILE__ .")!!!!\n";
-
 // set the timezone to avoid spurious errors from PHP
 date_default_timezone_set("America/Chicago");
 
 if(file_exists(__DIR__ .'/../vendor/autoload.php')) {
 	require_once(__DIR__ .'/../vendor/autoload.php');
 }
-
+else {
+	trigger_error("vendor autoloader not found, unit tests will probably fail -- try running 'composer update'");
+}
